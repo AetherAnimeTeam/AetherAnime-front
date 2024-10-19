c05b66dbffa0408ae820b26230068e354abba4c7
YFrite <bulatalimbekov5304@gmail.com>
2024-10-19 19:43:17 +1000

    change backend url

--- src/API/AnimeService.js
+++ src/API/AnimeService.js
@@ -2,13 +2,13 @@ import axios from "axios";
 
 export default class AnimeService {
     static async getPopular(limit = 10, page = 1){
-        return await axios.get("http://127.0.0.1:8000/popular/", {
+        return await axios.get("https://aetheranime-backend.onrender.com/popular/", {
             params: {limit: limit, page: page},
         });
     }
 
     static async getDetailed(anime_id) {
-        return await axios.get("http://127.0.0.1:8000/detailed/", {
+        return await axios.get("https://aetheranime-backend.onrender.com/detailed/", {
             params: {anime_id: parseInt(anime_id)}
         });
     }
